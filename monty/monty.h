ff38d257be6ebec1697a2af9ade6318cfe44cde6
Elvis Owusu-Sekyere <elvlon@yahoo.com>
2024-01-18 22:00:03 -0800

    hi

--- monty/monty.h
+++ monty/monty.h
@@ -34,6 +34,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void sub(stack_t **stack, unsigned int line_number);
+void div_op(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, int value);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
